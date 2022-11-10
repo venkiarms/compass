958139320999e5915eb6857b9158de798b7dd73e
venkiarms <venkiarms2391@gmail.com>
2022-11-10 10:29:03 +0530

    change get

--- server.js
+++ server.js
@@ -15,28 +15,34 @@ const addSchema = new mongoose.Schema({
     age: Number
 });
 //Collection
-const newModel = new mongoose.model('Data', addSchema);
-
-//insert data
+// const newModel = new mongoose.model('Data', addSchema);
+ 
+// // insert data
 // const data = new newModel({name:'Venki', age: 29})
 
 // // Save the data{name:'venki', age:23}
 // data.save()
 
-const data = async()=>{
+// const data = async()=>{
 
-    const result = await newModel.insertMany([
-        {name:'venki', age:23},
-        {name:'arun', age:23},
-        {name:'venkatesh', age:23}
+//     const result = await newModel.insertMany([
+//         {name:'venki', age:23},
+//         {name:'arun', age:23},
+//         {name:'venkatesh', age:23}
         
 
-    ])
-    console.log(result)
-}
-data();
+//     ])
+//     console.log(result)
+// }
+
+ 
+// data();
 
  
 app.listen(5000, ()=>{
+    console.log("Con in 5000");
+})
+
+app.get(5000, ()=>{
     console.log("Con in 5000");
 })
